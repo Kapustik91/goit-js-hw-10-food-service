007b9380450fffddfafd6ba5e3af473e4c67f512
Yuliia Kovalenko <kus_queen@icloud.com>
2020-10-16 23:12:20 +0300

    Added option to change themes

---
+++ my_hw10_project/src/changeTheme.js
@@ -0,0 +1,40 @@
+const body = document.querySelector('body');
+const switcher = document.querySelector('.js-switch-input');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+switcher.addEventListener('change', choiceOfTheme);
+switcher.addEventListener('change', onLocalStorage);
+
+function choiceOfTheme(el) {
+  const choice = switcher.checked;
+
+  if (choice) {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+  } else {
+    body.classList.add(Theme.LIGHT);
+    body.classList.remove(Theme.DARK);
+  }
+}
+
+function onLocalStorage(el) {
+  const choice = switcher.checked;
+
+  if (choice) {
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+const localTheme = localStorage.getItem('theme');
+
+if (localTheme === Theme.DARK) {
+  body.classList.add(Theme.DARK);
+  switcher.checked = true;
+}
--- my_hw10_project/src/index.js
+++ my_hw10_project/src/index.js
@@ -1,1 +1,3 @@
 import './styles.css';
+import menu from './menu.json';
+import './changeTheme.js';
