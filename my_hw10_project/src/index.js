e51841aa9926fe08e6d6556c72f4656a01b4ee8c
Yuliia Kovalenko <kus_queen@icloud.com>
2020-10-17 20:09:02 +0300

    Added menu

--- my_hw10_project/src/index.js
+++ my_hw10_project/src/index.js
@@ -1,3 +1,13 @@
-import './styles.css';
 import menu from './menu.json';
+import menuCards from './templates/menu-cards.hbs';
+import './styles.css';
 import './changeTheme.js';
+
+const menuEl = document.querySelector('.js-menu');
+menuEl.insertAdjacentElement('beforeend', menuMarkup);
+
+const menuMarkup = creatMenu(menu);
+
+function creatMenu(menu) {
+  return menuCards(menu);
+}
