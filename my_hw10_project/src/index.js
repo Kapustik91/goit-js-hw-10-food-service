5b848d91cf8c70dc7ae5c9b6fc700bbc06d3dcc5
Yuliia Kovalenko <kus_queen@icloud.com>
2020-10-18 21:22:41 +0300

    update

--- my_hw10_project/src/index.js
+++ my_hw10_project/src/index.js
@@ -1,7 +1,52 @@
 import menu from './menu.json';
 import menuCards from './templates/menu-cards.hbs';
 import './styles.css';
-import './changeTheme.js';
+// import './changeTheme.js';
+
+// change Theme
+
+const body = document.querySelector('body');
+const switcher = document.querySelector('.js-switch-input');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+switcher.addEventListener('change', choiceOfTheme);
+switcher.addEventListener('change', onLocalStorage);
+
+function choiceOfTheme(el) {
+  const choice = switcher.checked;
+
+  if (choice) {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+  } else {
+    body.classList.add(Theme.LIGHT);
+    body.classList.remove(Theme.DARK);
+  }
+}
+
+function onLocalStorage(el) {
+  const choice = switcher.checked;
+
+  if (choice) {
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+const localTheme = localStorage.getItem('theme');
+
+if (localTheme === Theme.DARK) {
+  body.classList.add(Theme.DARK);
+  switcher.checked = true;
+}
+
+// creat Menu
 
 const menuEl = document.querySelector('.js-menu');
 menuEl.insertAdjacentElement('beforeend', menuMarkup);
